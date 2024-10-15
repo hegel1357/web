c76aa1a251dbaa555e9c60dfff48e769d53021bd
hegel1357 <kevin900709@gmail.com>
2024-10-15 14:32:00 +0800

    add

--- calculator/calcutor.js
+++ calculator/calcutor.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-$('button').click(clicked)
-    function clicked() {
-        var char = $(this).text();
-        var result;
-        var screen_val = $('p').text();
-        if (char == 'C') {
-            result = '';
-        } else if (char == 'del') {
-            result = screen_val.slice(0, screen_val.length - 1);
-        }
-        else if (char == '=') {
-            try {
-                result = eval(screen_val);
-            } catch (e) {
-                $('p').text('Error');
-                alert(e)
+    $('button').click(clicked)
+        function clicked() {
+            var char = $(this).text();
+            var result;
+            var screen_val = $('p').text();
+            if (char == 'C') {
+                result = '';
+            } else if (char == 'del') {
+                result = screen_val.slice(0, screen_val.length - 1);
             }
-        } else {
-            result = screen_val + char;
+            else if (char == '=') {
+                try {
+                    result = eval(screen_val);
+                } catch (e) {
+                    $('p').text('Error');
+                    alert(e)
+                }
+            } else {
+                result = screen_val + char;
+            }
+            $('p').text(result);
         }
-        $('p').text(result);
-    }
-});
+    });
